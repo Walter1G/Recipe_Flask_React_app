3636e51e1e31f8b8b6338ea3b70f4dce128f47c9
Walter <2760walter@gmail.com>
2024-04-08 22:45:51 +0300

    add user model and use migrate to add user table

--- backend/models.py
+++ backend/models.py
@@ -24,4 +24,28 @@ class Recipe(db.Model):
         self.description=description        
         db.session.commit()
         
+
+
+class User(db.Model):
+    """class user
+    
+   id:interger
+   email:string
+   username:string
+   password:string
+    """
+    
+    __tablename__='users'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(25),nullable=False,unique=True)
+    email=db.Column(db.String(80), unique = True , nullable=False)
+    password=db.Column(db.Text(),nullable=False)
+    
+    
+    def __repr__(self) -> str:
+        return f"<User: {self.username} >"
+    
+    
+        
         
