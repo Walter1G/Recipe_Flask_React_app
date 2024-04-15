b28600d02fa1de7b9b55e92f691da88daf104833
Walter <2760walter@gmail.com>
2024-04-15 12:04:57 +0300

    client side sign up form validation

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {useForm} from 'react-hook-form'
 
 const SignUp = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
 
-  const submitForm = () => {
-    console.log("Form submitted");
-    console.log(username);
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
+  const { register, watch, handleSubmit, formState: { errors }, reset } = useForm();
 
+  const submitForm = (data) => {
+    console.log(data);
+    reset()
+  
 
-
-    setUsername('');
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
   };
+
+  console.log(watch("username"))
+  console.log(watch("email"))
+  console.log(watch("password"))
+  console.log(watch("confirmPassword"))
 
   return (
     <div className="container">
@@ -33,38 +33,39 @@ const SignUp = () => {
             <Form.Control
               type="text"
               placeholder="Enter UserName"
-              value={username}
-              name="username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
+            {...register('username',{required:true,maxLength:25})}
             />
+             <br></br>
+            {errors.username && <span style={{ color: "red" }}>Username is Required</span>}
+            <br></br>
+            {errors.username?.type=="maxLength"&&<span style={{ color: "red" }}>Max character should be 25</span>}
           </Form.Group>
+          
           <br></br>
           <Form.Group>
             <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
               placeholder="Enter your Email"
-              value={email}
-              name="email"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
+             {...register("email",{required:true,maxLength:80})}
             />
+            <br></br>
+            {errors.email && <span style={{ color: "red" }}>Email is Required</span>}
+            <br></br>
+            {errors.email?.type=="maxLength"&&<span style={{ color: "red" }}>Max character should be 80</span>}
           </Form.Group>
+         
           <br></br>
           <Form.Group>
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Enter password"
-              value={password}
-              name="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
+             {...register("password",{required:true,minLength:8})}
             />
+            <br></br>
+            {errors.password && <span style={{color:"red"}}>Password is Required</span>} <br></br>
+            {errors.password?.type=="minLength"&&<span style={{ color: "red" }}>Password should be at least 8 characters</span>}
           </Form.Group>
           <br></br>
           <Form.Group>
@@ -72,16 +73,16 @@ const SignUp = () => {
             <Form.Control
               type="password"
               placeholder="Confirm Password"
-              value={confirmPassword}
-              name="confirmPassword"
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
+              {...register("confirmPassword",{required:true,minLength:8})}
             />
+            <br></br>
+            {errors.confirmPassword && <span style={{ color: "red" }}>Please Confirm Password</span>}
+            <br></br>
+            {errors.confirmPassword?.type=="minLength"&&<span style={{ color: "red" }}>password should have at least 8 characters</span>}
           </Form.Group>
           <br></br>
           <Form.Group>
-            <Button as="sub" variant="primary" onClick={submitForm}>
+            <Button as="sub" variant="primary" onClick={handleSubmit(submitForm)}>
               Sign Up
             </Button>
           </Form.Group>
